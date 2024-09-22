cd1ad4a131e9c4daf699ae6f5875e516d19dab64
Sithija Ranaraja <sithija.vihanga28@gmail.com>
2024-09-22 19:28:11 +0530

    created custom data type for crowd flow predictor

--- goal_predictor/goal_predictor/crowd_flow_publisher.py
+++ goal_predictor/goal_predictor/crowd_flow_publisher.py
@@ -1,92 +1,93 @@
 import rclpy
 import numpy as np
-import matplotlib.pyplot    as plt
-import matplotlib.cm        as cm # Color Map
-from rclpy.node     import Node
-from std_msgs.msg   import Float32MultiArray
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm # Color Map
+from rclpy.node import Node
+#from std_msgs.msg import Float32MultiArray
+from smrr_interfaces.msg import Entities
 
 class CrowdFlowPublisher(Node):
 
     def __init__(self):
         super().__init__('crowd_flow_publisher')
-        self.publisher_     = self.create_publisher(Float32MultiArray, 'map_data', 10)
-        timer_period        = 0.2  # seconds
-        self.timer          = self.create_timer(timer_period, self.timer_callback)
+        self.publisher_ = self.create_publisher(Entities, 'map_data', 10)
+        timer_period = 0.2  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        self.num_agents         = 10   
-        self.goal_radius        = 0.5
-        self.time_step          = 0.05   # Simulation time
-        self.agent_radius       = 0.4
-        self.max_speed          = 1.0
-        self.num_obstacles      = 20
-        self.obstacle_radius    = 0.4
+        self.num_agents = 10   
+        self.goal_radius = 0.5
+        self.time_step = 0.05   # Simulation time
+        self.agent_radius = 0.4
+        self.max_speed = 1.0
+        self.num_obstacles = 5
+        self.obstacle_radius = 0.4
         self.avoidance_strength = 0.9 
 
-        np.random.seed(47) #20 #33
+        np.random.seed(58)
         self.positions  = np.random.rand(self.num_agents, 2) * 10
         self.goals      = np.random.rand(self.num_agents, 2) * 10
         self.obstacles  = np.random.rand(self.num_obstacles, 2) * 10
 
-        #############################################   Encoding system for data transfer ####################################################
-        #     [number of agents, number of obstacles, agent-01 X pos, agent-01 Y pos, ..., obstacle-01 X pos, obstacle-02 Y pos, ...]
-        #
-        ######################################################################################################################################
-        #TODO: Change the input data stream to agents and goals. Remove obstacles data.
-        self.mapData    = np.zeros(2 + 4*self.num_agents)  # num_goals = num_agents (Only used for validation)
-        self.mapData[0] = self.num_agents
-        self.mapData[1] = self.num_obstacles
+        # self.mapData = np.zeros(2 + 4 * self.num_agents)  
+        # self.mapData[0] = self.num_agents
+        # self.mapData[1] = self.num_obstacles
         
-        # Store paths for each agent
         self.paths = [self.positions[i:i+1].tolist() for i in range(self.num_agents)]
 
         # Visualization setup
         plt.ion()  # Activate interactive mode
-        fig, ax = plt.subplots()
-        ax.set_xlim(0, 10)
-        ax.set_ylim(0, 10)
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_title("Crowd flow simulator")
+        self.ax.set_xlim(0, 10)
+        self.ax.set_ylim(0, 10)
 
         colors = cm.get_cmap('tab10', self.num_agents)  # Get 10 distinct colors
 
-        self.agent_circles      = [self.create_circle(self.positions[i, 0], self.positions[i, 1], self.agent_radius, colors(i)) for i in range(self.num_agents)]
+        self.agent_circles = [self.create_circle(self.positions[i, 0], self.positions[i, 1], self.agent_radius, colors(i)) for i in range(self.num_agents)]
       
-        goals = []
+        self.goals_patches = []
         for i in range(self.num_agents):
-            goal = plt.Circle(self.goals[i], self.goal_radius, color='green')
-            goals.append(goal)
-            ax.add_patch(goals[i])
-        
-        obstacles = []
+            goal = plt.Circle(self.goals[i], self.goal_radius, color='green', alpha=0.5)
+            self.goals_patches.append(goal)
+            self.ax.add_patch(goal)
+
+        self.obstacles_patches = []
         for j in range(self.num_obstacles):
-            obstacle = plt.Circle(self.obstacles[j], self.obstacle_radius, color='black')
-            obstacles.append(obstacle)
-            ax.add_patch(obstacles[j])
-            
+            obstacle = plt.Circle(self.obstacles[j], self.obstacle_radius, color='black', alpha=0.3)
+            self.obstacles_patches.append(obstacle)
+            self.ax.add_patch(obstacle)  # Adding the obstacles
 
-        # Add labels inside the circles
-        self.goal_labels    = [ax.text(self.goals[i, 0], self.goals[i, 1], str(i), color='black', ha='center', va='center') for i in range(self.num_agents)]
-        self.agent_labels   = [ax.text(self.positions[i, 0], self.positions[i, 1], str(i), color='black', ha='center', va='center') for i in range(self.num_agents)]
+        self.goal_labels = [self.ax.text(self.goals[i, 0], self.goals[i, 1], str(i), color='black', ha='center', va='center') for i in range(self.num_agents)]
+        self.agent_labels = [self.ax.text(self.positions[i, 0], self.positions[i, 1], str(i), color='black', ha='center', va='center') for i in range(self.num_agents)]
 
-        # Initialize scatter plots 
-        self.path_dots = [ax.scatter([], [], c=colors(i), s=10, alpha=0.8) for i in range(self.num_agents)]
+        self.path_dots = [self.ax.scatter([], [], c=colors(i), s=30, alpha=0.8) for i in range(self.num_agents)]
+
+        # For arrows showing moving direction
+        self.arrows = [self.ax.arrow(self.positions[i, 0], self.positions[i, 1], 0.1, 0.1, head_width=0.2, color=colors(i)) for i in range(self.num_agents)]
+
+        # Footprints for agents
+        self.footprints = [self.ax.scatter([], [], c=colors(i), s=50, marker=(3, 0, 30), alpha=0.5) for i in range(self.num_agents)]
 
 
-    def timer_callback(self):
+    def timer_callback(self): 
+        msg = Entities()
+        msg.count = self.num_agents
+        msg.x = []
+        msg.y = []
         for j in range(self.num_agents):
-            self.mapData[2*j+2:2*j+4] = self.paths[j][-1]           # Add latest positions of human agents to mapData starting from 2nd position
-        self.mapData[2+2*self.num_agents:] = self.goals.flatten()   # Adding the goals to last part of mapData
-        msg = Float32MultiArray()
-        msg.data = tuple(self.mapData)
+            msg.x.append(self.paths[j][-1][0])
+            msg.y.append(self.paths[j][-1][1])
+
         self.publisher_.publish(msg)
-        self.crowd_flow()    # For visualization
+        self.crowd_flow()
 
     def create_circle(self, x, y, radius, color):
         return plt.Circle((x, y), radius, color=color, fill=False, lw=2)
-    
+
     def check_obstacle_collision(self, position, obstacle, obstacle_radius):
         distance = np.linalg.norm(position - obstacle)
-        return distance < obstacle_radius*2   # obstacle_radius + agent_radius
+        return distance < obstacle_radius * 2
 
-    # Adjust the agent's direction to avoid obstacles
     def avoid_obstacles(self, position, direction, obstacles):
         avoidance_vector = np.zeros(2)
         for obstacle in obstacles:
@@ -94,60 +95,63 @@ class CrowdFlowPublisher(Node):
                 avoidance_dir = position - obstacle
                 avoidance_distance = np.linalg.norm(avoidance_dir)
                 if avoidance_distance > 0:
-                    avoidance_dir /= avoidance_distance  # Normalize | Unit vector
-                    # Add to the avoidance vector with weight proportional to proximity
+                    avoidance_dir /= avoidance_distance  
                     avoidance_vector += avoidance_dir * (1 / avoidance_distance) * self.avoidance_strength
 
-        # Modify the direction based on the avoidance vector
-        adjusted_direction = direction + avoidance_vector # direction: direction to GOAL
+        adjusted_direction = direction + avoidance_vector  
         if np.linalg.norm(adjusted_direction) > 0:
-            adjusted_direction /= np.linalg.norm(adjusted_direction)  # Normalize
-        
+            adjusted_direction /= np.linalg.norm(adjusted_direction)  
         return adjusted_direction
 
     def update_positions(self, positions, goals):
         for i in range(self.num_agents):
-            # Vector towards the goal
             direction = goals[i] - positions[i]
             distance_to_goal = np.linalg.norm(direction)
-            
-            # Normalize direction
+
             if distance_to_goal > 0:
                 direction /= distance_to_goal
-            
-            # Adjust the direction to avoid obstacles
+
             direction = self.avoid_obstacles(positions[i], direction, self.obstacles)
-            
-            # Move towards the goal (or adjusted direction if avoiding obstacles)
             new_position = positions[i] + direction * self.max_speed * self.time_step
-            
-            # Check for collisions with other agents
+
             for j in range(self.num_agents):
                 if i != j:
                     distance = np.linalg.norm(new_position - positions[j])
-                    if distance < self.agent_radius * 2:  
+                    if distance < self.agent_radius * 2:
                         avoidance_direction = new_position - positions[j]
                         avoidance_direction /= np.linalg.norm(avoidance_direction)
-                        new_position += avoidance_direction * (self.agent_radius * 2 - distance)  # To change the strength
+                        new_position += avoidance_direction * (self.agent_radius * 2 - distance)
 
             self.positions[i] = new_position
-            self.paths[i].append(new_position.tolist())  
-    
+            self.paths[i].append(new_position.tolist())
+
     def crowd_flow(self):
         self.update_positions(self.positions, self.goals)
-        
+
         for i in range(self.num_agents):
             self.agent_circles[i].center = self.positions[i]
             self.agent_labels[i].set_position(self.positions[i])
 
             path_positions = np.array(self.paths[i])
-            num_path_dots = len(path_positions)
-            fade = np.linspace(0, 1, num_path_dots)  # Gradual fade effect
-            self.path_dots[i].set_offsets(path_positions)
-            self.path_dots[i].set_alpha(fade)
+            reduced_path_positions = path_positions[::3]  # Reduce the number of dots for the path
+            self.path_dots[i].set_offsets(reduced_path_positions)
 
+            # Update the direction arrows to point to the current movement direction
+            if len(self.paths[i]) > 1:
+                current_direction = self.positions[i] - self.paths[i][-2]
+                self.arrows[i].remove()  # Remove the old arrow
+                self.arrows[i] = self.ax.arrow(self.positions[i, 0], self.positions[i, 1], current_direction[0], current_direction[1], head_width=0.2, color=self.agent_circles[i].get_edgecolor())
+
+            # Update the footprints around agents
+            self.footprints[i].set_offsets(np.array(self.paths[i])[-4:])  # Show last few positions for footprint effect
+
+        # Redraw the obstacles
+        for j in range(self.num_obstacles):
+            self.obstacles_patches[j].center = self.obstacles[j]  # Ensure the obstacle position is updated
+            
         plt.draw()
         plt.pause(0.01)
+
 
 def main(args=None):
     rclpy.init(args=args)
