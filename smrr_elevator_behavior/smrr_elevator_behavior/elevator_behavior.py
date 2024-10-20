d4aba977900e4794a724ade7ce72694ed4787564
Sithija Ranaraja <sithija.vihanga28@gmail.com>
2024-10-20 21:36:01 +0530

    Adjusted behavior tree template

--- smrr_elevator_behavior/smrr_elevator_behavior/elevator_behavior.py
+++ smrr_elevator_behavior/smrr_elevator_behavior/elevator_behavior.py
@@ -5,58 +5,76 @@ from py_trees.composites import Sequence
 from py_trees import logging as log_tree
 
 
-class Action(Behaviour):
+class LineEstimation(Behaviour):
   def __init__(self, name):
-    super(Action, self).__init__(name)
+    super(LineEstimation, self).__init__(name)
 
   def setup(self):
-    self.logger.debug(f"Action::setup {self.name}")
+    self.logger.debug(f"LineEstimation::setup {self.name}")
 
   def initialise(self):
-    self.logger.debug(f"Action::initialise {self.name}")
+    self.logger.debug(f"LineEstimation::initialise {self.name}")
 
   def update(self):
-    self.logger.debug(f"Action::update {self.name}")
+    self.logger.debug(f"LineEstimation::update {self.name}")
     sleep(1)
     return Status.SUCCESS
 
   def terminate(self, new_status):
-    self.logger.debug(f"Action::terminate {self.name} to {new_status}")
+    self.logger.debug(f"LineEstimation::terminate {self.name} to {new_status}")
 
 
-class Condition(Behaviour):
+class ButtonEstimation(Behaviour):
   def __init__(self, name):
-    super(Condition, self).__init__(name)
+    super(ButtonEstimation, self).__init__(name)
 
   def setup(self):
-    self.logger.debug(f"Condition::setup {self.name}")
+    self.logger.debug(f"ButtonEstimation::setup {self.name}")
 
   def initialise(self):
-    self.logger.debug(f"Condition::initialise {self.name}")
+    self.logger.debug(f"ButtonEstimation::initialise {self.name}")
 
   def update(self):
-    self.logger.debug(f"Condition::update {self.name}")
+    self.logger.debug(f"ButtonEstimation::update {self.name}")
     sleep(1)
     return Status.SUCCESS
 
   def terminate(self, new_status):
-    self.logger.debug(f"Condition::terminate {self.name} to {new_status}")
+    self.logger.debug(f"ButtonEstimation::terminate {self.name} to {new_status}")
+
+
+class ButtonLocalization(Behaviour):
+  def __init__(self, name):
+    super(ButtonLocalization, self).__init__(name)
+
+  def setup(self):
+    self.logger.debug(f"ButtonLocalization::setup {self.name}")
+
+  def initialise(self):
+    self.logger.debug(f"ButtonLocalization::initialise {self.name}")
+
+  def update(self):
+    self.logger.debug(f"ButtonLocalization::update {self.name}")
+    sleep(1)
+    return Status.SUCCESS
+
+  def terminate(self, new_status):
+    self.logger.debug(f"ButtonLocalization::terminate {self.name} to {new_status}")
 
 
 def make_bt():
-  root = Sequence(name="sequence", memory=True)
+  root = Sequence(name="elevator_interaction", memory=True)
 
-  check_battery = Condition("check_battery")
-  open_gripper = Action("open_gripper")
-  approach_object = Action("approach_object")
-  close_gripper = Action("close_gripper")
+  button_estimation     = ButtonEstimation("estimate_button")
+  line_estimation       = LineEstimation("estimate_line")
+  button_localization   = LineEstimation("button_localization")
+ 
 
   root.add_children(
       [
-          check_battery,
-          open_gripper,
-          approach_object,
-          close_gripper
+          button_estimation,
+          line_estimation,
+          button_localization
       ]
   )
 
